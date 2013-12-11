9a20b3285987fc954f5da64e3f3482dd44e25d74
Jacob Overgaard <overgaard@gmail.com>
2013-12-11 09:01:47 +0100

    Dir structure

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'susy'
+
+http_path = "/"
+css_dir = "build/css"
+sass_dir = "lib/sass"
+images_dir = "lib/images"
+generated_images_dir = "build/images"
+relative_assets = true
